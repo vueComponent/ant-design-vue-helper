833d37cd4ec6f7075ff1cba418ef5eafe1f13586
chsword <chsword@126.com>
2020-06-15 17:59:29 +0800

    fixed a-select-option

--- build/specialAttributesMap.ts
+++ build/specialAttributesMap.ts
@@ -7,5 +7,6 @@ const specialAttributesMap: Record<string, string> = {
   "a-datepicker-common": "a-date-picker",
   "mentions/a-option": "a-mentions-option",
   "meta/a-meta": "a-list-item-meta",
+  "select/a-option": "a-select-option",
 };
 export default specialAttributesMap;
