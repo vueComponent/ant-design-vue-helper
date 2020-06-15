d9588544450e41b2c497921b01d22bc351551a15
chsword <chsword@126.com>
2020-06-15 18:07:06 +0800

    fixed a-select-opt-group a-table-column a-table-column-group a-tooltip

--- build/specialAttributesMap.ts
+++ build/specialAttributesMap.ts
@@ -8,5 +8,9 @@ const specialAttributesMap: Record<string, string> = {
   "mentions/a-option": "a-mentions-option",
   "meta/a-meta": "a-list-item-meta",
   "select/a-option": "a-select-option",
+  "select/a-option-group": "a-select-opt-group",
+  "table/a-column": "a-table-column",
+  "table/a-column-group": "a-table-column-group",
+  "a-tooltip-common": "a-tooltip",
 };
 export default specialAttributesMap;
